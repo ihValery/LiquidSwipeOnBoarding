8a850f84442424e3b1f0b96d1e13aa79dc052936
ihValery <ihValery@gmail.com>
2021-11-02 22:03:13 +0300

    Start

--- LiquidSwipeOnBoarding/View/HomeView.swift
+++ LiquidSwipeOnBoarding/View/HomeView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  HomeView.swift
 //  LiquidSwipeOnBoarding
 //
 //  Created by Валерий Игнатьев on 02.11.2021.
@@ -7,15 +7,14 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HomeView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        HomeView()
     }
 }
