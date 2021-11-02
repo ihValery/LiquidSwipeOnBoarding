96e241b217d3c9d9f9578c70192a7c2ab3ce7bf8
ihValery <ihValery@gmail.com>
2021-11-02 21:13:18 +0300

    Initial Commit

---
+++ LiquidSwipeOnBoarding/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LiquidSwipeOnBoarding
+//
+//  Created by Валерий Игнатьев on 02.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LiquidSwipeOnBoarding/LiquidSwipeOnBoardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiquidSwipeOnBoardingApp.swift
+//  LiquidSwipeOnBoarding
+//
+//  Created by Валерий Игнатьев on 02.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct LiquidSwipeOnBoardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
