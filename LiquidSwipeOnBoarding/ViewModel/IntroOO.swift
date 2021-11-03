3ae3c948ae8b877f05532fdc5f0adebd31148009
ihValery <ihValery@gmail.com>
2021-11-03 06:41:28 +0300

    Final

--- LiquidSwipeOnBoarding/ViewModel/IntroOO.swift
+++ LiquidSwipeOnBoarding/ViewModel/IntroOO.swift
@@ -9,7 +9,7 @@ import Foundation
 
 class IntroOO: ObservableObject {
     @Published var intros: [IntroDO] = [
-        IntroDO(title: "Мы", subTitle: "Анализировали", description: "Мы много работали, чтобы найти решение.", pic: "picOne", color: .orangeOne),
+        IntroDO(title: "Мы", subTitle: "Анализировали", description: "Много работали, чтобы найти решение.", pic: "picOne", color: .orangeOne),
         IntroDO(title: "Мы", subTitle: "Исследовали", description: "Искали проблемы которые нуждались в решении.", pic: "picTwo", color: .blueOne),
         IntroDO(title: "Мы", subTitle: "Проектировали", description: "Разработали и решили.", pic: "picThree", color: .purpleLightOne)]
 //        IntroDO(title: "We", subTitle: "WORKED", description: "We worked hard ti find a solution. Тестовое слово.", pic: "picOne", color: .purpleLightOne),
